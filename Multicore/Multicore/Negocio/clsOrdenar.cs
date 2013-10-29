9ce05c91fdd0008134ffad35a71b76765b37f557
Jorge Rojas Aragonés <jeragones@gmail.com>
2013-10-29 16:41:32 -0600

    ordenar

--- Multicore/Multicore/Negocio/clsOrdenar.cs
+++ Multicore/Multicore/Negocio/clsOrdenar.cs
@@ -14,13 +14,13 @@ namespace Multicore.Negocio
             return _loLista;
         }
 
-        public List<object[]> mergeSort(List<object[]> _loLista)
+        public List<object[]> mergeSort(List<object[]> _loLista, int _iColumna)
         {
-            mergeSort(_loLista, 0, _loLista.Count - 1);
+            mergeSort(_loLista, 0, _loLista.Count - 1, _iColumna);
             return null;
         }
 
-        private void mergeSort(List<object[]> _loLista, int _iInicio, int _iFin) 
+        private static void mergeSort(List<object[]> _loLista, int _iInicio, int _iFin, int _iColumna) 
         {
             //Condicion de parada
             if (_iInicio == _iFin)
@@ -29,20 +29,17 @@ namespace Multicore.Negocio
             int iMitad = (_iInicio + _iFin) / 2;
             //Voy a ordenar recursivamente la primera mitad
             //y luego la segunda
-            mergeSort(_loLista, _iInicio, iMitad);
-            mergeSort(_loLista, iMitad + 1, _iFin);
+            _loLista mergeSort(_loLista, _iInicio, iMitad, _iColumna);
+            mergeSort(_loLista, iMitad + 1, _iFin, _iColumna);
             //Mezclo las dos mitades ordenadas
-            List<object[]> liAux = merge(_loLista, _iInicio, iMitad, iMitad + 1, _iFin);
-            
+            object[] liAux = merge(_loLista, _iInicio, iMitad, iMitad + 1, _iFin, _iColumna);
             //Array.Copy(liAux, 0, _loLista, _iInicio, liAux.Count);
         }
 
-        private List<object[]> merge(List<object[]> _loLista, int _iInicio1, int _iFin1, int _iInicio2, int _iFin2)
+        private static object[] merge(List<object[]> _loLista, int _iInicio1, int _iFin1, int _iInicio2, int _iFin2, int _iColumna)
         {
-            int a_ = _iInicio1;
-            int b_ = _iInicio2;
             int iTmp = _iFin1 - _iInicio1 + _iFin2 - _iInicio2 + 2;
-            List<object[]> loResult = new List<object[]>();
+            object[] loResult = new object[iTmp];
 
             for (int i = 0; i < iTmp; i++)
             {
@@ -63,7 +60,7 @@ namespace Multicore.Negocio
                         // s1 = s2 : 0
                         // s1 > s2 : 1
                         // s1 < s2 : -1
-                        if (((string)_loLista[_iInicio2][0]).CompareTo((string)_loLista[_iInicio1][0]) == -1) 
+                        if (((string)_loLista[_iInicio2][1]).CompareTo((string)_loLista[_iInicio1][1]) == -1) 
                         //if (_loLista[_iInicio2] <= _loLista[_iInicio1])
                         {
                             loResult[i] = _loLista[_iInicio2];
